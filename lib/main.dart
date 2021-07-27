49fb308bb181962c806a51233ffde413630297f6
Piyush wani <piyushwani04@gmail.com>
2021-07-27 12:16:27 +0530

    add ai_util file

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp(   
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
